cf9813ef7bb7cb2c64e61e9d489b2c4137fdfea4
Bora <98665128+borademirtas@users.noreply.github.com>
2022-02-24 03:27:57 -0600

    Create 122A - Lucky Division.cpp

---
+++ 122A - Lucky Division.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h> 
+
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    if (n % 4 == 0 || n % 7 == 0 || n % 47 == 0 || n % 74 == 0 || n % 474 == 0 || n % 447 == 0 || n % 477 == 0 || n % 774 == 0 || n % 747 == 0) {cout << "YES";}
+    else { cout << "NO"; }
+}
