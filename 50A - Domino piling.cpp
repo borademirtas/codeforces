057a25e7ea921952cbaf0354749d2735b3f4b9f5
Bora <98665128+borademirtas@users.noreply.github.com>
2022-02-24 03:09:26 -0600

    Create 50A - Domino piling.cpp

---
+++ 50A - Domino piling.cpp
@@ -0,0 +1,11 @@
+    #include <iostream>
+    #include <string>
+    #define ll long long 
+    #include <math.h> 
+    using namespace std;
+     
+    int main() {
+    	ll m, n;
+    	cin >> m >> n;
+    	cout << m * n / 2;
+    }
