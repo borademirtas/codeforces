353470bf8c44660641cd7c1c4b8277a8b3c5447f
Bora <98665128+borademirtas@users.noreply.github.com>
2022-04-14 14:15:15 -0500

    Create 1178C - Tiles.cpp

---
+++ 1178C - Tiles.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>  
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <bitset>
+#include <math.h>
+#include <map>
+#include <set>
+#include <numeric>
+#include <queue>
+using namespace std;
+#define ll long long 
+#define vll vector<ll>
+#define pbb push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define MOD % 998244353
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve();}
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll map<ll,ll> b;
+#define cout std::cout
+
+
+int main() {
+	FAST;
+	ll w, h, ans = 1;
+	cin >> w >> h;
+	while (w--) { ans *= 2; ans = ans MOD; }
+	while (h--) { ans *= 2; ans = ans MOD; }
+	cout << ans MOD;
+}
+	
+	
