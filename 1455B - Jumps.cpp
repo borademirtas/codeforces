30e5cd4fc0aafaff693d79114efe3762bb35830e
Bora <98665128+borademirtas@users.noreply.github.com>
2022-04-22 17:41:28 -0500

    Create 1455B - Jumps.cpp

---
+++ 1455B - Jumps.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>  
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <bitset>
+#include <math.h>
+#include <map>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <list>
+using namespace std;
+#define ll long long 
+#define vll vector<ll>
+#define PB push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve();}
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll map<ll,ll>;
+#define cout std::cout
+#define vll2 vector<vector<ll>> a(n, vector<ll>(m));	
+
+void solve() {
+	ll n, jumps = 0;
+	cin >> n;
+	while (n > 0) {
+		n -= ++jumps;
+	}
+	if (n == -1) { jumps++; }
+	cout << jumps << "\n";
+}
+
+int main() {
+	FAST;
+	tc;
+}
+	
