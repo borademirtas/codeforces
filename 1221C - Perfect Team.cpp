75e19f7ade83b68db3400cac97b65263192f0943
Bora <98665128+borademirtas@users.noreply.github.com>
2022-04-25 20:33:25 -0500

    Create 1221C - Perfect Team.cpp

---
+++ 1221C - Perfect Team.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>  
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <bitset>
+#include <math.h>
+#include <map>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <list>
+#include <deque>
+#include <iomanip>
+using namespace std;
+#define ll long long 
+#define vll vector<ll>
+#define PB push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve();}
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll map<ll,ll>;
+#define cout std::cout
+#define vll2 vector<vector<ll>> a(n, vector<ll>(m));
+void solve() {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    cout << min(min(a, b), (a + b + c) / 3) << '\n';
+
+}
+int main() {
+    FAST;
+    tc;
+}
