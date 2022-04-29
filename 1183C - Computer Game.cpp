77b6f74f76f418eee97044440a068c4a1eed2f15
Bora <98665128+borademirtas@users.noreply.github.com>
2022-04-29 01:41:37 -0500

    Create 1183C - Computer Game.cpp

---
+++ 1183C - Computer Game.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>  
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <bitset>
+#include <math.h>
+#include <map>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <list>
+#include <deque>
+#include <climits>
+#include <iomanip>
+using namespace std;
+#define ll long long 
+#define vll vector<ll>
+#define PB push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve();}
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll map<ll,ll>
+#define cout std::cout
+#define vll2 vector<vector<ll>> a(n, vector<ll>(m));
+
+void solve(){
+    ll k, n, a, b;
+    cin >> k >> n >> a >> b;
+    k--;
+    if (k / b < n) { cout << -1 << "\n"; return; }
+    ll best = n * b;
+    ll dif = a - b;
+    cout << min(((k - best) / dif), n) << "\n";
+}
+
+int main() {
+    FAST;
+    tc;
+}
