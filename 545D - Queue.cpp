2bdba946bad8e4aaea9eea70838bfe50ab0fdbb1
Bora <borademirtas@yahoo.com>
2022-05-13 10:24:39 -0500

    Create 545D - Queue.cpp

---
+++ 545D - Queue.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <map>
+#include <cmath>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <list>
+#include <deque>
+#include <climits>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+#define ll long long
+#define vll vector<ll>
+#define PB push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve();}
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll map<ll,ll>
+#define cout std::cout
+#define vll2 vector<vector<ll>> a(n, vector<ll>(m));
+#define FIXED(A) cout << std::fixed << A << std::endl;
+
+
+int main() {
+    FAST;
+    ll n;
+    cin >> n;
+    vfast;
+    sort(a.begin(), a.end());
+    ll time = 0, ans = 0;
+    for(ll i = 0; i < n; i++){
+        if(a[i] >= time){time += a[i]; ans++;}
+    }
+    cout << ans;
+}
