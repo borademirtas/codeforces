06445e7817ffc8e0b34735201e4f497bb22c5901
Bora <borademirtas@yahoo.com>
2022-06-08 18:37:48 -0500

    Create 1526C2 - Potions (Hard Version).cpp

---
+++ 1526C2 - Potions (Hard Version).cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <map>
+#include <cmath>
+#include <unordered_map>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <list>
+#include <deque>
+#include <climits>
+#include <iomanip>
+#include <fstream>
+#include <stack>
+#include <bitset>
+using namespace std;
+#define ll long long
+#define vll vector<ll>
+#define pb push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve();}
+#define tc1 solve();
+#define mpvll map<ll, vll>
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll unordered_map<ll,ll>
+#define cout std::cout
+#define vll2 vector<vector<ll>> a(n, vector<ll>(m));
+#define FIXED(A) cout.precision(20); cout << (A) << "\n";
+#define mod 1000000007
+#define mp(A, B) make_pair(A, B)
+#define all(x) (x).begin(), (x).end()
+ll rounddiv(ll x, ll y){ll ans = x / y; if(x % y != 0){ans++;} return ans;}
+
+
+void solve(){
+    ll n;
+    cin >> n;
+    vll a(n);
+    for(ll i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    ll pc = 0, hc = 0;
+    priority_queue<ll> q;
+    for(ll i = 0; i < n; i++){
+        pc++;
+        hc += a[i];
+        q.push(a[i] * -1);
+        while(hc < 0){
+            pc--;
+            hc += q.top();
+            q.pop();
+        }
+    }
+    cout << pc;
+}
+
+int main() {
+    FAST;
+    tc1;
+}
