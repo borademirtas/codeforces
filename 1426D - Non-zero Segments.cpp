179bf42b76766e0571c23eeb459f6b010c76f9df
Bora <borademirtas@yahoo.com>
2022-06-18 18:37:09 -0500

    Create 1426D - Non-zero Segments.cpp

---
+++ 1426D - Non-zero Segments.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <map>
+#include <cmath>
+#include <unordered_map>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <list>
+#include <deque>
+#include <climits>
+#include <iomanip>
+#include <fstream>
+#include <stack>
+#include <bitset>
+using namespace std;
+#define ll long long
+#define vll vector<ll>
+#define pb push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve(i+1);}
+#define tc1 solve(1);
+#define mpvll map<ll, vll>
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll unordered_map<ll,ll>
+#define cout std::cout
+#define vll2 vector<vector<ll>> a(n, vector<ll>(m));
+#define FIXED(A) cout.precision(20); cout << (A) << "\n";
+#define mod 1000000007
+#define mp(A, B) make_pair(A, B)
+#define all(x) (x).begin(), (x).end()
+ll rounddiv(ll x, ll y){ll ans = x / y; if(x % y != 0){ans++;} return ans;}
+
+void solve(ll TC){
+    ll n;
+    cin >> n;
+    vll a(n);
+    for (ll i = 0; i < n; i++) { cin >> a[i]; }
+    ll total = 0;
+    mpll b;
+    b[0]++;
+    ll ans = 0;
+    for(ll i = 0; i < n; i++){
+        total += a[i];
+        if(b[total] > 0){ans++; b.clear(); b[0]++; total = a[i];}
+        b[total]++;
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    FAST;
+    tc1;
+}
