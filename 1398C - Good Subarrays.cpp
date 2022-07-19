a5cc958f49a46128e2aab0d525c4072195c74aab
Bora <borademirtas@yahoo.com>
2022-07-18 20:53:05 -0500

    Create 1398C - Good Subarrays.cpp

---
+++ 1398C - Good Subarrays.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <map>
+#include <cmath>
+#include <unordered_map>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <list>
+#include <deque>
+#include <climits>
+#include <iomanip>
+#include <fstream>
+#include <stack>
+#include <bitset>
+using namespace std;
+#define ll long long
+#define vll vector<ll>
+#define pb push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve();}
+#define tc1 solve();
+#define mpvll map<ll, vll>
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll unordered_map<ll,ll>
+#define cout std::cout
+#define vll2 vector<vector<ll>> a(n, vector<ll>(m));
+#define FIXED(A) cout.precision(20); cout << A << "\n";
+#define mod 1000000007
+#define mp(A, B) make_pair(A, B)
+#define all(x) (x).begin(), (x).end()
+ll rounddiv(ll x, ll y){ll ans = x / y; if(x % y != 0){ans++;} return ans;}
+
+void solve(){
+    ll n;
+    string s;
+    cin >> n >> s;
+    mpll b;
+    ll count = 0;
+    ll sum = 0;
+    for(ll i = 0; i < n; i++) {
+        ll num = s[i] - 48;
+        ll place = i + 1;
+        sum += num;
+        count += b[sum - i];
+        if(sum == place){count++;}
+        b[sum - i]++;
+    }
+    cout << count << "\n";
+}
+
+int main() {
+    FAST;
+    tc;
+}
