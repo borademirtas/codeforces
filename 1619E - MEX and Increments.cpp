a141c375a8b3b16b72c5380e900877caed6e7aff
Bora <borademirtas@yahoo.com>
2022-08-08 11:43:18 -0500

    Create 1619E - MEX and Increments.cpp

---
+++ 1619E - MEX and Increments.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <map>
+#include <cmath>
+#include <unordered_map>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <list>
+#include <deque>
+#include <climits>
+#include <iomanip>
+#include <fstream>
+#include <stack>
+#include <bitset>
+using namespace std;
+#define ll long long
+#define vll vector<ll>
+#define pb push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve(i, t);}
+#define tc1 solve(1, 1);
+#define mpvll map<ll, vll>
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll map<ll,ll>
+#define cout std::cout
+#define vll2 vector<vector<ll>> a(n, vector<ll>(m));
+#define FIXED(A) cout.precision(20); cout << A << "\n";
+#define mod 1000000007
+#define mp(A, B) make_pair(A, B)
+#define all(x) (x).begin(), (x).end()
+#define tri pair<ll, pair<ll,ll>>
+ll rounddiv(ll x, ll y){ll ans = x / y; if(x % y != 0){ans++;} return ans;}
+
+void solve(ll TC, ll TC2){
+    ll n;
+    cin >> n;
+    ll un = 0;
+    vll a(n);
+    vll c(n+1);
+    for(ll i= 0 ; i < n; i++){cin >> a[i]; c[a[i]]++;}
+    priority_queue<ll> q;
+    ll sum = 0;
+    ll ok = true;
+    for(ll i = 0; i <= n; i++){
+        if(!ok){cout << -1 << " "; continue;}
+        for(ll j = 1; j  < c[i]; j++){q.push(i);}
+        cout  << sum + c[i] << " ";
+        if(c[i] == 0){
+            if(q.empty()){ok = false;}
+            if(!q.empty()){
+                sum += i - q.top();
+                q.pop();
+            }
+        }
+    }
+    cout << "\n";
+}
+
+int main() {
+    FAST;
+    tc;
+}
