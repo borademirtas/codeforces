d3a45cbff4c7b7cc7796046314e4b60b7f6d8253
Bora <borademirtas@yahoo.com>
2022-08-21 22:12:12 -0500

    Create 1260C - Infinite Fence.cpp

---
+++ 1260C - Infinite Fence.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <map>
+#include <cmath>
+#include <unordered_map>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <list>
+#include <deque>
+#include <climits>
+#include <iomanip>
+#include <fstream>
+#include <stack>
+#include <bitset>
+using namespace std;
+#define ll long long
+#define vll vector<ll>
+#define pb push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve(i, t);}
+#define tc1 solve(1, 1);
+#define mpvll map<ll, vll>
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll map<ll,ll>
+#define cout std::cout
+#define vll2 vector<vector<ll>> a(n, vector<ll>(m));
+#define FIXED(A) cout.precision(20); cout << A << "\n";
+#define mod 1000000007
+#define mp(A, B) make_pair(A, B)
+#define all(x) (x).begin(), (x).end()
+#define tri pair<ll, pair<ll,ll>>
+ll rounddiv(ll x, ll y){ll ans = x / y; if(x % y != 0){ans++;} return ans;}
+
+
+
+void solve(ll TC, ll TC2) {
+    ll x, y, k;
+    cin >> x >> y >> k;
+    ll g = gcd(x, y);
+    x /= g; y /= g;
+    if(x > y){swap(x, y);}
+    if((k-1) * x + 1 < y){cout << "REBEL\n";}
+    else{cout << "OBEY\n";}
+}
+
+int main() {
+    FAST;
+    tc;
+}
