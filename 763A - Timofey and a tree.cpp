406c7ad8cb2687e7af32572ce3e7b97f19d9c209
Bora <borademirtas@yahoo.com>
2022-10-19 17:03:04 -0500

    Create 763A - Timofey and a tree.cpp

---
+++ 763A - Timofey and a tree.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <map>
+#include <cmath>
+#include <unordered_map>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <list>
+#include <deque>
+#include <climits>
+#include <iomanip>
+#include <cstring>
+#include <fstream>
+#include <stack>
+#include <bitset>
+using namespace std;
+#define ll long long
+#define  vll vector<ll>
+#define pb push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve(i, t);}
+#define tc1 solve(1, 1);
+#define mpvll map<ll, vll>
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll map<ll,ll>
+#define vll2 vector<vector<ll>> dp(n, vector<ll>(m));
+#define FIXED(A) cout << fixed; cout.precision(20); cout << A << "\n";
+#define mp(A, B) make_pair(A, B)
+#define all(x) (x).begin(), (x).end()
+#define tri pair<ll, pair<ll,ll>>
+void setIO(string name) {ios_base::sync_with_stdio(0); cin.tie(0);freopen((name+".in").c_str(),"r",stdin);freopen((name+".out").c_str(),"w",stdout);}
+ll rd(ll a, ll b){return (a+b-1) / b;}
+ll isqrt(ll x){ll ans = 0; for(ll k = 1LL << 30; k != 0; k /= 2){if ((ans + k) * (ans + k) <= x) {ans += k;} } return ans;   }
+vll prime(ll x){ll n = x; vll ans; while (n % 2 == 0) {ans.pb(2);n = n/2;} for (int i = 3; i <= sqrt(n); i = i + 2) {while (n % i == 0){ans.pb(i);n = n/i;}}if (n > 2){ans.pb(n);} return ans;}
+ll inverse(ll a){ll m = 998244353; ll m0 = m; ll y = 0, x = 1; while (a > 1) { ll q = a / m; ll t = m; m = a % m, a = t; t = y; y = x - q * y; x = t;}if (x < 0){x += m0;} return x;}
+ll binpow(ll a, ll b, ll m) { a %= m; ll res = 1; while (b > 0) { if (b & 1){res = res * a % m;}a = a * a % m; b >>= 1;} return res;}
+
+vll parent(1e5+1, 1);
+
+void make_set(int v) {
+    parent[v] = v;
+}
+
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b)
+        parent[b] = a;
+}
+
+
+void solve(ll TC, ll TC2) {
+    ll n; cin >> n;
+    vector<vll> adj(n+1);
+    vll color(n+1);
+    for(ll i = 0; i < parent.size(); i++){parent[i] = i;}
+    for(ll i = 0; i < n - 1; i++){
+        ll x, y; cin >> x >> y;
+        adj[x].pb(y); adj[y].pb(x);
+    }
+    for(ll i =1; i <= n; i++){cin >> color[i];}
+    for(ll i = 1; i <= n; i++){
+        for(ll j = 0; j < adj[i].size(); j++){
+            if(color[i] == color[adj[i][j]]){
+                union_sets(i, adj[i][j]);
+            }
+        }
+    }
+    vll cnt(n+1);
+    for(ll i = 1; i <= n; i++){
+        cnt[find_set(i)]++;
+    }
+    for(ll i = 1; i <= n; i++){
+        cnt[find_set(i)]--;
+        ll num = 0; vll pos;
+        for(ll j = 0; j < adj[i].size(); j++){
+            pos.pb(find_set(adj[i][j]));
+        }
+        sort(all(pos));
+        for(ll j = 0; j < pos.size(); j++) {
+            if(j != 0 && pos[j] != pos[j-1]){
+                num += cnt[pos[j]];
+            }
+            if(j == 0){num += cnt[pos[j]];}
+        }
+        if(num == n -1){cout << "YES\n" << i; return;}
+        cnt[find_set(i)]++;
+    }
+    cout << "NO";
+}
+
+int main() {
+    FAST;
+    tc1;
+}
