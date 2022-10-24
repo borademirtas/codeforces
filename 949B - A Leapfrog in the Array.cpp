645f314dae6b02d22aa9becf36c7427a86f3e8b4
Bora <borademirtas@yahoo.com>
2022-10-23 22:44:56 -0500

    Create 949B - A Leapfrog in the Array.cpp

---
+++ 949B - A Leapfrog in the Array.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define  vll vector<ll>
+#define pb push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve(i, t);}
+#define tc1 solve(1, 1);
+#define mpvll map<ll, vll>
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll map<ll,ll>
+#define vll2 vector<vector<ll>> dp(n, vector<ll>(m));
+#define FIXED(A) cout << fixed; cout.precision(20); cout << A << "\n";
+#define mp(A, B) make_pair(A, B)
+#define all(x) (x).begin(), (x).end()
+#define tri pair<ll, pair<ll,ll>>
+void setIO(string name) {ios_base::sync_with_stdio(0); cin.tie(0);freopen((name+".in").c_str(),"r",stdin);freopen((name+".out").c_str(),"w",stdout);}
+ll rd(ll a, ll b){return (a+b-1) / b;}
+ll isqrt(ll x){ll ans = 0; for(ll k = 1LL << 30; k != 0; k /= 2){if ((ans + k) * (ans + k) <= x) {ans += k;} } return ans;   }
+vll prime(ll x){ll n = x; vll ans; while (n % 2 == 0) {ans.pb(2);n = n/2;} for (int i = 3; i <= sqrt(n); i = i + 2) {while (n % i == 0){ans.pb(i);n = n/i;}}if (n > 2){ans.pb(n);} return ans;}
+ll binpow(ll a, ll b, ll m) { a %= m; ll res = 1; while (b > 0) { if (b & 1){res = res * a % m;}a = a * a % m; b >>= 1;} return res;}
+ll lg2(ll n){ll cnt=0;while(n){cnt++;n/=2;}return cnt;}
+
+
+ll calc(ll x, ll  n){
+    if(x&1){return (x+1)/2;}
+    ll pre = x / 2;
+    ll rem = n - pre - 1;
+    return calc(x + rem +1, n);
+}
+
+void solve(ll TC, ll TC2) {
+    ll n, q;
+    cin >> n >> q;
+    for(ll i = 0; i < q; i++){
+        ll x; cin >> x;
+        cout << calc(x, n) << "\n";
+    }
+}
+
+int main() {
+    FAST;
+    tc1;
+}
