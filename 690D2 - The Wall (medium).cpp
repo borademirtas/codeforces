bc261f1b1fe3f5359b2c64fa1b86dca5779a0e62
Bora <borademirtas@yahoo.com>
2022-10-25 07:22:31 -0500

    Create 690D2 - The Wall (medium).cpp

---
+++ 690D2 - The Wall (medium).cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define  vll vector<ll>
+#define pb push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve(i, t);}
+#define tc1 solve(1, 1);
+#define mpvll map<ll, vll>
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll map<ll,ll>
+#define vll2 vector<vector<ll>> dp(n, vector<ll>(m));
+#define FIXED(A) cout << fixed; cout.precision(20); cout << A << "\n";
+#define mp(A, B) make_pair(A, B)
+#define all(x) (x).begin(), (x).end()
+#define tri pair<ll, pair<ll,ll>>
+void setIO(string name) {ios_base::sync_with_stdio(0); cin.tie(0);freopen((name+".in").c_str(),"r",stdin);freopen((name+".out").c_str(),"w",stdout);}
+ll rd(ll a, ll b){return (a+b-1) / b;}
+ll isqrt(ll x){ll ans = 0; for(ll k = 1LL << 30; k != 0; k /= 2){if ((ans + k) * (ans + k) <= x) {ans += k;} } return ans;   }
+vll prime(ll x){ll n = x; vll ans; while (n % 2 == 0) {ans.pb(2);n = n/2;} for (int i = 3; i <= sqrt(n); i = i + 2) {while (n % i == 0){ans.pb(i);n = n/i;}}if (n > 2){ans.pb(n);} return ans;}
+ll binpow(ll a, ll b, ll m) { a %= m; ll res = 1; while (b > 0) { if (b & 1){res = res * a % m;}a = a * a % m; b >>= 1;} return res;}
+ll lg2(ll n){ll cnt=0;while(n){cnt++;n/=2;}return cnt;}
+
+
+
+vll fac(1e6);
+
+ll inverse(ll a){
+    ll m = 1e6+3; ll m0 = m; ll y = 0, x = 1;
+    //if (m == 1){return 0;}
+    while (a > 1) { ll q = a / m; ll t = m; m = a % m, a = t; t = y; y = x - q * y; x = t;}
+    if (x < 0){x += m0;}
+    return x;
+}
+
+
+long long ncr(int n, int k) {
+    ll m = 1e6 + 3;
+    return fac[n] * inverse(fac[k] * fac[n - k] % m) % m;
+}
+
+
+
+void solve(ll TC, ll TC2) {
+    ll m = 1e6 + 3;
+    fac[0] = 1; for(ll i = 1; i < 1e6; i++){fac[i] = fac[i-1] * i; fac[i] %= m;}
+    ll ans = 0; ll n, c; cin >> n >> c;
+    for(ll i = 1; i <= n; i++){
+        ans += ncr(i+c-1, i);
+
+        ans %= m;
+    }
+    cout << ans;
+}
+
+int main() {
+    FAST;
+    tc1;
+}
