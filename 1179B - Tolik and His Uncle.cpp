483497b399ad95f655d1ff3350f31f246946dfba
Bora <borademirtas@yahoo.com>
2022-11-21 01:20:10 -0600

    Create 1179B - Tolik and His Uncle.cpp

---
+++ 1179B - Tolik and His Uncle.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define  vll vector<ll>
+#define pb push_back
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+#define vp vector<pair<ll,ll>>
+#define tc ll t; cin >> t; for(ll i = 0; i < t; i++){solve(i, t);}
+#define tc1 solve(1, 1);
+#define mpvll map<ll, vll>
+#define vfast vll a(n); for (ll i = 0; i < n; i++) { cin >> a[i]; }
+#define mpll map<ll,ll>
+#define vll2 vector<vector<ll>> dp(n, vector<ll>(m));
+#define FIXED(A) cout << fixed; cout.precision(20); cout << A << "\n";
+#define mp(A, B) make_pair(A, B)
+#define all(x) (x).begin(), (x).end()
+struct tri{ll x = 0, y = 0, z = 0;};
+template <typename num_t>
+using ordered_set = tree<num_t, null_type, less_equal<num_t>, rb_tree_tag, tree_order_statistics_node_update>;
+void setIO(string name) {freopen((name+".in").c_str(),"r",stdin);freopen((name+".out").c_str(),"w",stdout);}
+ll rd(ll a, ll b){return (a+b-1) / b;}
+ll isqrt(ll x){ll ans = 0; for(ll k = 1LL << 30; k != 0; k /= 2){if ((ans + k) * (ans + k) <= x) {ans += k;} } return ans;   }
+vll prime(ll x){ll n = x; vll ans; while (n % 2 == 0) {ans.pb(2);n = n/2;} for (int i = 3; i <= sqrt(n); i = i + 2) {while (n % i == 0){ans.pb(i);n = n/i;}}if (n > 2){ans.pb(n);} return ans;}
+ll binpow(ll a, ll b, ll m) { a %= m; ll res = 1; while (b > 0) { if (b & 1){res = res * a % m;}a = a * a % m; b >>= 1;} return res;}
+ll lg2(ll n){ll cnt=0;while(n){cnt++;n/=2;}return cnt;}
+
+
+
+void solve(ll TC, ll TC2) {
+    ll n, m; cin >> n >> m;
+    vector<deque<ll>> a(n);
+    for(ll i = 0; i < n; i++){for(ll j = 0; j < m; j++){a[i].push_back(j);}}
+    ll l = 0, r = n -1;
+    if(n == 2 && m == 3){
+        cout << 1 << " " << 1 << "\n";
+        cout << 1 << " " << 3 << "\n";
+        cout << 1 << " " << 2 << "\n";
+        cout << 2 << " " << 2 << "\n";
+        cout << 2 << " " << 3 << "\n";
+        cout << 2 << " " << 1<< "\n";
+        return;
+    }
+    while(r >= l){
+        while(!a[l].empty() && !a[r].empty()){
+            cout << l + 1 << " " << a[l].front() + 1 << "\n"; a[l].pop_front();
+            if(!a[r].empty()){cout << r + 1 << " " << a[r].back() + 1 << "\n"; a[r].pop_back();}
+        }
+        r--; l++;
+    }
+}
+
+int main() {
+    FAST;
+    tc1;
+}
